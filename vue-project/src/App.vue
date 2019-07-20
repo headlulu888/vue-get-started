e3f3a2ed6a9fab256d4c6c8518e9626b2c756314
headlulu888 <headlulu888@gmail.com>
2019-07-20 22:52:26 +0300

    parameters in directives

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
-      <h1 v-colored v-if="visible">{{ title }}</h1>      
-      <button @click="visible = !visible">Toggle</button>
-      <button @click="title = 'New title'">Change title</button>
+      <h1 v-colored="'red'">{{ title }}</h1>
   </div>
 </template>
 
@@ -10,8 +8,7 @@
 export default {
   data() {
     return {
-      title: 'hello i am vue',
-      visible: true
+      title: 'hello i am vue'
     }
   }
 }
