6d04eb7e7f738b95b3c81754073118a3b70e1d83
headlulu888 <headlulu888@gmail.com>
2019-07-21 22:09:38 +0300

    Filtering lists

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
       <h1>{{ title }}</h1>
-      <h1>{{ title | lowercase }}</h1>
-      <h1>{{ title | uppercase }}</h1>
+
+      <input type="text" v-model="searchName">
+
+      <ul>
+        <li v-for="name of filteredNames">{{ name }}</li>
+      </ul>
   </div>
 </template>
 
@@ -10,7 +14,18 @@
 export default {
   data() {
     return {
-      title: 'HellO i am vue'
+      title: 'HellO i am vue',
+      searchName: '',
+      names: [
+        'vlad', 'max', 'elena', 'igor'
+      ]
+    }
+  },
+  computed: {
+    filteredNames() {
+      return this.names.filter(name => {
+        return name.toLowerCase().indexOf(this.searchName.toLowerCase()) !== -1
+      })
     }
   },
   filters: {
