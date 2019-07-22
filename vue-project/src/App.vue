25fcefddaf34034282cbf0ca6b73617d81500764
headlulu888 <headlulu888@gmail.com>
2019-07-22 23:32:13 +0300

    text input

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,34 +1,20 @@
 <template>
   <div>
-      <h1>{{ title }}</h1>
-
-      <input type="text" v-model="searchName">
-
-      <ul>
-        <li v-for="name of filteredNames">{{ name }}</li>
-      </ul>
-      <hr>
-      <app-list></app-list>
+      <h2>test</h2>
+      <input type="text" v-model.lazy="name">
+      <p>{{ name }}</p>
   </div>
 </template>
 
 <script>
-import ListMixin from './listMixin'
-
 export default {
   data() {
     return {
-      title: 'HellO i am vue'
-    }
-  },
-  mixins: [ListMixin],
-  filters: {
-    lowercase(value) {
-      return value.toLowerCase()
+      name: 'initial state'
     }
   }
 }
 </script>
 
-<style>
+<style scoped>
 </style>
