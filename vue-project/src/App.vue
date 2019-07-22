98416184b6cfa109a67b91a15003e6152fff1069
headlulu888 <headlulu888@gmail.com>
2019-07-22 23:45:35 +0300

    checkbox

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,8 +1,23 @@
 <template>
   <div>
       <h2>test</h2>
-      <textarea v-model="textarea">{{ textarea }}</textarea>
-      <p>{{ textarea }}</p>
+      <label for="">
+        <input type="checkbox" value="instagram" v-model="socials">Instagram
+      </label>
+
+      <label for="">
+        <input type="checkbox" value="vk" v-model="socials">Vk
+      </label>
+
+      <label for="">
+        <input type="checkbox" value="facebook" v-model="socials">Facebook
+      </label>
+
+      <hr>
+
+      <ul>
+        <li v-for="s of socials">{{ s }}</li>
+      </ul>
   </div>
 </template>
 
@@ -10,7 +25,9 @@
 export default {
   data() {
     return {
-      textarea: 'initial state'
+      socials: [
+        'vk'
+      ]
     }
   }
 }
