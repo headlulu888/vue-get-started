a4297099649250a9e07024c703f8a21ba795c29e
headlulu888 <headlulu888@gmail.com>
2019-07-28 23:11:29 +0300

    add password validators

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -14,22 +14,39 @@
         <div class="invalid-feedback" v-if="!$v.email.required">Email field is required</div>
         <div class="invalid-feedback" v-if="!$v.email.email">This field should be an email</div>
       </div>
+
+      <div class="form-group pt-3">
+        <label for="password">Password</label>
+        <input 
+          type="password" 
+          id="password" 
+          class="form-control"
+          :class="{'is-invalid': $v.password.$error}"
+          @blur="$v.password.$touch()"
+          v-model="password"
+        >
+        <div class="invalid-feedback" v-if="!$v.password.minLength">Min length of password is {{ $v.password.$params.minLength.min }}. Now it is {{ password.length }}</div>
+      </div>
     </form>
   </div>
 </template>
 
 <script>
-import { required, email } from 'vuelidate/lib/validators'
+import { required, email, minLength } from 'vuelidate/lib/validators'
 export default {
   data() {
     return {
-      email: ''
+      email: '',
+      password: ''
     }
   },
   validations: {
     email: {
       required,
       email
+    },
+    password: {
+      minLength: minLength(6)
     }
   }
 }
