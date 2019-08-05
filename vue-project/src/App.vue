bd28e5d828b52c95cc125b79b4b3b954d997acc6
headlulu888 <headlulu888@gmail.com>
2019-08-05 23:44:31 +0300

    create navigation bar

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <router-link class="nav-item nav-link" :to="'/'">Home</router-link>
+          <router-link class="nav-item nav-link" :to="'/cars'">Cars</router-link>
+        </div>
+      </div>
+    </nav>
+
     <router-view></router-view>
   </div>
 </template>
