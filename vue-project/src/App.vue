60e8b25687eaf01a6aeb5b179b8eb65030b8bbf8
headlulu888 <headlulu888@gmail.com>
2019-08-06 23:39:18 +0300

    active link designation

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,10 +2,20 @@
   <div class="container">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <router-link class="nav-item nav-link" :to="'/'">Home</router-link>
-          <router-link class="nav-item nav-link" :to="'/cars'">Cars</router-link>
-        </div>
+        <ul class="navbar-nav">
+          <!-- <li class="nav-item active">
+            <router-link class="nav-link active" :to="'/'">Home</router-link>
+          </li> -->
+          <!-- <li class="nav-item">
+            <router-link class="nav-item nav-link" :to="'/cars'">Cars</router-link>
+          </li> -->
+          <router-link tag="li" class="nav-item" exact to="/" active-class="active">
+            <a class="nav-link">Home</a>
+          </router-link>
+          <router-link tag="li" class="nav-item" to="/cars">
+            <a class="nav-link">Cars</a>
+          </router-link>
+        </ul>
       </div>
     </nav>
 
