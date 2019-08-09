488073c3f17545769db12ffb7f0bc77b858067ef
headlulu888 <headlulu888@gmail.com>
2019-08-09 23:21:57 +0300

    passing parameters on router

--- vue-project/src/pages/CarFull.vue
+++ vue-project/src/pages/CarFull.vue
@@ -1,12 +1,18 @@
 <template>
     <div>
-        <h4>Car name: Ford</h4>
-        <h5>Car year: 2016</h5>
+        <!-- Два способа вывода -->
+        <h4>Car name: {{ $route.query.name }}</h4>
+        <h5>Car year: {{ year }}</h5>
     </div>
 </template>
 
 <script>
 export default {
+    computed: {
+        year() {
+            return this.$route.query.year
+        }
+    }
 }
 </script>
 
