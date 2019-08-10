72354401ac3344b44b062559d818cf2ee40aca38
headlulu888 <headlulu888@gmail.com>
2019-08-10 23:46:57 +0300

    add settings for vue-resource

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,16 +1,12 @@
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import App from './App.vue'
-import Vuelidate from 'vuelidate'
-import VueRouter from 'vue-router'
-import router from './routes'
 
-Vue.use(VueRouter)
-Vue.use(Vuelidate)
+Vue.use(VueResource)
 
 new Vue({
   el: '#app',
   render: h => h(App),
-  router: router
 })
 
 
