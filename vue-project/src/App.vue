2aed4a1f7cf4f3bac2695bce4782f21972741642
headlulu888 <headlulu888@gmail.com>
2019-08-11 10:52:19 +0300

    Getting GET Objects

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -11,6 +11,19 @@
     </div>
 
     <button class="btn btn-success" @click="createCar">Create car</button>
+    <button class="btn btn-primary" @click="loadCars">Load cars</button>
+
+    <hr>
+
+    <ul class="list-group">
+      <li 
+        class="list-group-item"
+        v-for="car of cars"
+        :key="car.id"
+      >
+        <strong>{{car.name}}</strong> - {{ car.year }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -19,7 +32,8 @@ export default {
   data() {
     return {
       carName: '',
-      carYear: 2018
+      carYear: 2018,
+      cars: []
     }
   },
   methods: {
@@ -36,6 +50,15 @@ export default {
         })
         .then(newCar => {
           console.log(newCar)
+        })
+    },
+    loadCars() {
+      this.$http.get('http://localhost:3000/cars')
+        .then(response => {
+          return response.json()
+        })
+        .then(cars => {
+          this.cars = cars
         })
     }
   }
