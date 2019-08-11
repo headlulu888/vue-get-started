53e1f2c27237909016a0432c9e86c502fee14cb8
headlulu888 <headlulu888@gmail.com>
2019-08-11 11:24:17 +0300

    Add Interceptors

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -3,7 +3,10 @@ import VueResource from 'vue-resource'
 import App from './App.vue'
 
 Vue.use(VueResource)
-Vue.http.options.root = 'http://localhost:3000/';
+Vue.http.options.root = 'http://localhost:3000/'
+Vue.http.interceptors.push(request => {
+  request.headers.set('Auth', 'RAND TOKEN ' + Math.random())
+})
 
 new Vue({
   el: '#app',
