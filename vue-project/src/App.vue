fe02cb390784c6c8cb76bd6ba07f1d7aae0718ae
headlulu888 <headlulu888@gmail.com>
2019-08-12 23:22:43 +0300

    add  vuex

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,64 +1,23 @@
 <template>
-  <div class="container pt-2">
-    <div class="form-group">
-      <label for="name">Car Name</label>
-      <input type="text" id="name" class="form-control" v-model.trim="carName">
-    </div>
-
-    <div class="form-group">
-      <label for="year">Car Year</label>
-      <input type="text" id="year" class="form-control" v-model.number="carYear">
-    </div>
-
-    <button class="btn btn-success" @click="createCar">Create car</button>
-    <button class="btn btn-primary" @click="loadCars">Load cars</button>
-
-    <hr>
-
-    <ul class="list-group">
-      <li 
-        class="list-group-item"
-        v-for="car of cars"
-        :key="car.id"
-      >
-        <strong>{{car.name}}</strong> - {{ car.year }}
-      </li>
-    </ul>
+  <div class="container text-center pt-5">
+    <app-counter :counter="counter"></app-counter>
+    <app-actions @counterUpdated="counter += $event"></app-actions>
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      carName: '',
-      carYear: 2018,
-      cars: [],
-      resource: null
-    }
-  },
-  methods: {
-    createCar() {
-      const car = {
-        name: this.carName,
-        year: this.carYear
+  import Counter from './Counter'
+  import Actions from './Actions'
+
+  export default {
+    data() {
+      return {
+        counter: 0
       }
-
-      this.resource.save({}, car)
     },
-    loadCars() {
-      this.resource.get()
-        .then(response => {
-          return response.json()
-        })
-        .then(cars => this.cars = cars)
+    components: {
+      appCounter: Counter,
+      appActions: Actions
     }
-  },
-  created() {
-    this.resource = this.$resource('cars')
   }
-}
 </script>
-
-<style>
-</style>
